57d0967fb3daa8f1aff088220fd839c7e5af76cf
l1storez <alesrov@gmail.com>
2021-03-07 16:15:29 +0200

    Tell sphinks parallelism is safe

--- pypa_theme/__init__.py
+++ pypa_theme/__init__.py
@@ -5,3 +5,8 @@ def setup(app):
     current_dir = os.path.abspath(os.path.dirname(__file__))
     app.add_html_theme(
         'pypa_theme', current_dir)
+
+    return {
+        'parallel_read_safe': True,
+        'parallel_write_safe': True,
+    }
